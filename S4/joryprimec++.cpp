81d5e2fdeaa32a47b5f6e826a9826798c620717f
miguelsysdata <miguel.sysdata@gmail.com>
2026-09-18 07:23:21 -0500

    Update joryprimec++.cpp
    a

--- S4/joryprimec++.cpp
+++ S4/joryprimec++.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<fstream>
+#include<conio.h>
+#include<string.h>
 
 using std::cout;
 using std::cin;
@@ -14,25 +16,43 @@ using std::string;
 	
 main(){
 	
-	int n;
-	FILE *p, *q, *r;	
-	estudiante curso[10];
+	FILE *p;	
+	estudiante curso[10], aux;
+	int pmay=0;
+	
+	if(p=fopen("estudiantes.txt","r")){
 		
-	p = fopen("estudiantes.txt", "r");
-	q = fopen("aprobados.txt", "w");
-	r = fopen("reprobados.txt", "w");
+		for(int i=0; i<10; i++){
+			fscanf(p, "%d %s %f", &curso[i].cod, &curso[i].nombre, &curso[i].prom);
+		}
+		fclose(p);
+		
+		//mostrar datos
+		for(int i=0; i<10; i++)
+			cout<<"\n"<<curso[i].cod<<"\t"<<curso[i].nombre<<"\t"<<curso[i].prom;
+		
+		for(int i=0; i<10; i++){
+			pmay=i;
+			for(int j=i+1; j<10; j++){
+				if(curso[j].prom>curso[pmay].prom)
+					pmay=j;
+			}
+			aux=curso[i];
+			curso[i]=curso[pmay];
+			curso[pmay]=aux;
+		}
+		
+		cout<<"\n______________________________\n"
+			<<"\nDatos ordenados por promedio: "
+			<<"\n______________________________\n";
+		for(int i=0; i<10; i++){
+			cout<<"\n"<<curso[i].cod<<"\t"<<curso[i].nombre<<"\t"<<curso[i].prom;
+			getch();
+		}
+			
+			
+	}else
+		cout<<"No se pudo abrir el archivo..."<<endl;
 	
-	for(int i=0; i<10; i++){
-		fscanf(p, "%d %s %f", &curso[i].cod, &curso[i].nombre, &curso[i].prom);
-	}
-	
-	while(!feof(p)){
-		fscanf(q, "%f %s %f", &n);
-	
-	}
-	
-	fclose(p);
-	for(int i=0; i<10; i++)
-		cout<<"\n"<<curso[i].cod<<"\t"<<curso[i].nombre<<"\t"<<curso[i].prom;
 		
 }
